db05de623833a700d5daa510080839c4deea1679
Satoru SATOH <satoru.satoh@gmail.com>
2017-02-06 18:16:40 +0900

    enhancement: update the experimental code to convert nested dicts to relations

--- m9dicts/relations.py
+++ m9dicts/relations.py
@@ -2,10 +2,13 @@
 # Copyright (C) 2017 Red Hat, Inc.
 # License: MIT
 #
+# Suppress warning about name of namedtuple objects:
+# pylint: disable=invalid-name
 """Flatten nested dicts, etc.
 """
 from __future__ import absolute_import
 
+import collections
 import itertools
 import operator
 
@@ -13,119 +16,158 @@ import m9dicts.compat
 import m9dicts.utils
 
 
-def object_to_id(obj):
-    """Object -> id.
+Ref = collections.namedtuple("Ref", "relvar id".split())
+ID_KEY = "id"
+
+
+def object_to_id(obj, digits=12):
+    """
+    ..note:: Maybe collision occurs depends on `digits`
 
     :param obj: Any object has __str__ method to get its ID value
+    :param digits: number of digists to take from generated hex digest str
 
     >>> object_to_id("test")
-    '098f6bcd4621d373cade4e832627b4f6'
+    127077368941
     >>> object_to_id({'a': "test"})
-    'c5b846ec3b2f1a5b7c44c91678a61f47'
+    262814736276
     >>> object_to_id(['a', 'b', 'c'])
-    'eea457285a61f212e4bbaaf890263ab4'
+    317209567448
     """
-    return m9dicts.compat.md5(m9dicts.compat.to_str(obj)).hexdigest()
+    nid = int(m9dicts.compat.md5(m9dicts.compat.to_str(obj)).hexdigest(), 16)
+    return int(str(nid)[:digits])
 
 
 def _gen_id(*args):
     """
-    :return: ID generated from `args`
+    :return: ID (long int) value generated from `args`
     """
-    return object_to_id(args)
+    return object_to_id(sorted(args))
 
 
-def _rel_name(rel_name, key, level=0, names=None):
+def _gen_relvar(*args):
     """
-    :return: Generated composite relation name from relations
+    :return: Generated ID string
     """
-    if names is None:
-        names = []
-    name = "rel_%s_%s" % (rel_name, key)
-    if name in names:
-        name = "%s_%d" % (name, level)
-    return name
+    return '_'.join(args)
 
 
-def _sorted(items):
+def _rank_1_dict_to_rels(dic, relvar=None):
     """
-    :param items: Any iterables
+    Convert a rank 1, not nested, dict to tuples of (relvar, tuples) where
+    relvar represents the name of the relations and tuples are a list of tuples
+    each represents a relation between key and value.
+
+    :param dic: A dict or dict-like object maybe nested.
+    :param relvar: Relation variable
+
+    :return: A tuple of (<relvar>, [tuple of key and value])
+
+    >>> _rank_1_dict_to_rels(dict(a=1, id=2), "data")
+    ('data', (('a', 1), ('id', 2)))
+    >>> _rank_1_dict_to_rels(dict(a=1, id=2))
+    ('a_id', (('a', 1), ('id', 2)))
+    >>> _rank_1_dict_to_rels(dict(a=1))  # doctest: +ELLIPSIS
+    ('a', (('a', 1), ('id', 224491522528)))
     """
-    if items and m9dicts.utils.is_dict_like(items[0]):
-        # return items  # Return as it is because dicts are not ordered.
-        return sorted(items, key=lambda d: list(d.items()))
-    else:
-        return sorted(items)
+    if relvar is None:
+        relvar = _gen_relvar(*sorted(dic.keys()))
+
+    if "id" not in dic:
+        dic["id"] = _gen_id(*sorted(dic.items()))
+
+    return (relvar, tuple(sorted((k, v) for k, v in dic.items())))
 
 
-def _dict_to_rels_itr_0(dic, key, rel_name, pid, **kwargs):
+def _tuple_id(tpl):
     """
-    :param dic: A dict or dict-like object
-    :param key: Key name
-    :param rel_name: Name for parent relations
-    :param pid: ID of parent object
-    :param kwargs: Keyword arguments such as level, names
+    Get the ID of given tuple `tpl`.
+
+    :param tpl: (relvar, (tuple_0, ...))
     """
-    cid = dic.get("id", _gen_id(*sorted(dic.items())))
-    name = _rel_name(rel_name, key, **kwargs)
-    yield (name, ((key, cid), (rel_name, pid)))
+    try:
+        tid = [t for t in tpl[1] if t[0] == "id"][0][1]
+        return (tpl[0], tid)  # (relvar, <id>)
+    except IndexError:
+        return None
 
-    for tpl in _dict_to_rels_itr(dic, key, **kwargs):
-        yield tpl
 
-
-def _dict_to_rels_itr(dic, rel_name, level=0, names=None):
+def _is_new_and_update_seen(tpl, seen):
     """
-    Convert nested dict[s] to tuples of relation name and relations of items in
-    the dict, and yields each pairs.
-
-    :param dic: A dict or dict-like object
-    :param rel_name: Name for relations of items in `dic`
-    :return: A list of (<relation_name>, [tuple of key and value])
-
-    >>> list(_dict_to_rels_itr(dict(id=0, a=1, b="b"), "ab"))
-    [('ab', (('id', 0), ('a', 1), ('b', 'b')))]
-
-    >>> list(_dict_to_rels_itr(dict(id=0, a=dict(id=1, b=1), d="D"),
-    ...                        "A"))  # doctest: +NORMALIZE_WHITESPACE
-    [('A', (('id', 0), ('d', 'D'))),
-     ('rel_A_a', (('a', 1), ('A', 0))),
-     ('a', (('id', 1), ('b', 1)))]
+    Search a tuple `tpl` from seen and update it as needed.
     """
-    lkeys = [k for k, v in dic.items() if m9dicts.utils.is_list_like(v)]
-    dkeys = [k for k, v in dic.items() if m9dicts.utils.is_dict_like(v)]
-    items = sorted((k, v) for k, v in dic.items()
-                   if k != "id" and k not in lkeys and k not in dkeys)
-    pid = dic.get("id", _gen_id(*items))
-    yield (rel_name, tuple([("id", pid)] + items))
+    tid = _tuple_id(tpl)  # (relvar, id), id is not None.
+    if tid not in seen:
+        seen.add(tid)
+        return True
 
-    if names is None:
-        names = []
-    else:
-        names.append(rel_name)
+    return False
 
-    level += 1
-    kwargs = dict(level=level, names=names)
-    if lkeys:
-        for key in sorted(lkeys):
-            for val in _sorted(dic[key]):
-                if m9dicts.utils.is_dict_like(val):
-                    # :todo: Avoid name collision.
-                    # if name in val:
-                    #     ...
-                    for tpl in _dict_to_rels_itr_0(val, key, rel_name, pid,
-                                                   **kwargs):
-                        yield tpl
-                else:
-                    cid = _gen_id(key, val)
-                    yield (_rel_name(rel_name, key, **kwargs),
-                           (("id", cid), (rel_name, pid), (key, val)))
 
-    if dkeys:
-        for key in sorted(dkeys):
-            for tpl in _dict_to_rels_itr_0(dic[key], key, rel_name, pid,
-                                           **kwargs):
+def _is_list_item(val):
+    """
+    :return: True if given `val` is a list of items or False
+    """
+    return (val and m9dicts.utils.is_list_like(val) and
+            not isinstance(val, Ref))
+
+
+def _ndict_to_rels_itr(dic, seen, relvar=None, idkey=ID_KEY):
+    """
+    Convert a dict to tuples of (relvar, tuples) where relvar represents the
+    name of the relations and tuples are a list of tuples of each tuple
+    represents a relation between key and value, and yields tuples one by one.
+
+    .. note:: Yielded relations may be duplicated.
+
+    :param dic: A dict or dict-like object maybe nested
+    :param seen: A set object to search items seen previously
+    :param relvar: Relation variable
+    :param idkey: Key to identify the item
+
+    :return: A list of (<relvar>, [tuple of key and value])
+
+    >>> f = lambda *args: sorted(_ndict_to_rels_itr(*args))
+    >>> s = set()
+    >>> f(dict(A=dict(id=0, a=1), id=1),
+    ...   s, "X")  # doctest: +NORMALIZE_WHITESPACE
+    [('A', (('a', 1), ('id', 0))),
+     ('X', (('A', Ref(relvar='A', id=0)), ('id', 1)))]
+    """
+    if not dic:
+        return  # `dic` is empty.
+
+    if relvar is None:
+        relvar = _gen_relvar(*sorted(k for k in dic.keys() if k != "id"))
+
+    rdic = dic.copy()
+    for key, val in dic.items():
+        if m9dicts.utils.is_dict_like(val):
+            crelvar = "%s.%s" % (relvar, key) if key == relvar else key
+            if idkey in val:
+                refid = val[idkey]
+            else:
+                refid = _gen_id(*sorted(val.items()))
+                val[idkey] = refid
+
+            rdic[key] = Ref(crelvar, refid)  # Replace rdic[key] with Ref.
+            for tpl in _ndict_to_rels_itr(val, seen, relvar=crelvar,
+                                          idkey=idkey):
                 yield tpl
+
+    litems = ((k, v) for k, v in rdic.items() if _is_list_item(v))
+    for key, val in litems:
+        for item in val:  # Clone and yield each relations later.
+            ldic = rdic.copy()
+            ldic[key] = item  # Replace ldic[key] :: list with item
+            for tpl in _ndict_to_rels_itr(ldic, seen, relvar=relvar,
+                                          idkey=idkey):
+                yield tpl
+        return
+
+    tpl = _rank_1_dict_to_rels(rdic, relvar=relvar)
+    if _is_new_and_update_seen(tpl, seen):
+        yield tpl
 
 
 def dict_to_rels(dic, name=None):
@@ -135,16 +177,12 @@ def dict_to_rels(dic, name=None):
 
     :param dic: A dict or dict-like object
     :param name: Name for relations of items in `dic`
-    :return: A list of (<relation_name>, [tuple of key and value])
+    :return: A list of (<relvar>, [tuple of key and value or Ref object])
     """
-    assert m9dicts.utils.is_dict_like(dic)
-
-    if name is None:
-        name = "data"  # Default.
-
     fst = operator.itemgetter(0)
-    rels = _dict_to_rels_itr(dic, name)
-    return [(k, sorted(t[1] for t in g))
+    seen = set()
+    rels = _ndict_to_rels_itr(dic, seen, relvar=name)
+    return [(k, sorted(set(t[1] for t in g)))
             for k, g in itertools.groupby(sorted(rels, key=fst), fst)]
 
 # vim:sw=4:ts=4:et:
--- m9dicts/tests/relations.py
+++ m9dicts/tests/relations.py
@@ -7,50 +7,37 @@ import unittest
 import m9dicts.relations as TT
 
 
-class Test_10_functions(unittest.TestCase):
+def _ndict_to_rels(*args, **kwargs):
+    return sorted(TT._ndict_to_rels_itr(*args, **kwargs))
 
-    def test_10_dict_to_rels_itr__simple(self):
-        dic = dict(id=0, a=1, b="b")
-        ref = [('ab', (('id', 0), ('a', 1), ('b', 'b')))]
-        self.assertEqual(list(TT._dict_to_rels_itr(dic, "ab")), ref)
 
-    def test_12_dict_to_rels_itr__lists(self):
-        dic = dict(id=0, a=1, b=[2, 3], c="c")
-        id_0 = TT._gen_id('b', 2)
-        id_1 = TT._gen_id('b', 3)
-        ref = [('ac', (('id', 0), ('a', 1), ('c', 'c'))),
-               ('rel_ac_b', (('id', id_0), ('ac', 0), ('b', 2))),
-               ('rel_ac_b', (('id', id_1), ('ac', 0), ('b', 3)))]
-        self.assertEqual(list(TT._dict_to_rels_itr(dic, "ac")), ref)
+class Test_20__ndict_to_rels_itr(unittest.TestCase):
+    """
+    >>> f = _ndict_to_rels
+    >>> s = set(); f(dict(A=dict(id=0, a=1), id=1),
+    ...   s, "data")  # doctest: +NORMALIZE_WHITESPACE
+    [('A', (('a', 1), ('id', 0))),
+     ('data', (('A', Ref(relvar='A', id=0)), ('id', 1)))]
 
-    def test_14_dict_to_rels_itr__lists(self):
-        dic = dict(id='01', a=1, b=[2, 3], c=["c"])
-        id_0 = TT._gen_id('b', 2)
-        id_1 = TT._gen_id('b', 3)
-        id_2 = TT._gen_id('c', 'c')
-        ref = [('A', (('id', '01'), ('a', 1))),
-               ('rel_A_b', (('id', id_0), ('A', '01'), ('b', 2))),
-               ('rel_A_b', (('id', id_1), ('A', '01'), ('b', 3))),
-               ('rel_A_c', (('id', id_2), ('A', '01'), ('c', 'c')))]
-        self.assertEqual(list(TT._dict_to_rels_itr(dic, "A")), ref)
+    >>> s = set(); f(dict(A=dict(a=1, id=1)),
+    ...              s)  # doctest: +NORMALIZE_WHITESPACE
+    [('A', (('A', Ref(relvar='A.A', id=1)), ('id', 285990133928))),
+     ('A.A', (('a', 1), ('id', 1)))]
 
-    def test_16_dict_to_rels_itr__lists_of_dicts(self):
-        dic = dict(id=0, a="AAA",
-                   b=[dict(id=0, b=1, c=2), dict(id=1, b=0, c=3)])
-        rest = sorted([('b', (('id', 0), ('b', 1), ('c', 2))),
-                       ('b', (('id', 1), ('b', 0), ('c', 3))),
-                       ('rel_A_b', (('b', 0), ('A', 0))),
-                       ('rel_A_b', (('b', 1), ('A', 0)))])
+    >>> s = set(); f(dict(A=dict(a=1)),
+    ...              s)  # doctest: +NORMALIZE_WHITESPACE
+    [('A', (('A', Ref(relvar='A.A', id=224491522528)), ('id', 227711280590))),
+     ('A.A', (('a', 1), ('id', 224491522528)))]
 
-        ref = [('A', (('id', 0), ('a', 'AAA')))] + rest
-        res = list(TT._dict_to_rels_itr(dic, "A"))
-        self.assertEqual(ref[0], res[0])
-        self.assertEqual(sorted(ref[1:]), sorted(res[1:]))
+    >>> f(dict(A=[dict(a=1, id=0)]),
+    ...   s, "A")  # doctest: +NORMALIZE_WHITESPACE, +ELLIPSIS
+    [('A', (('A', Ref(relvar='A.A', id=0)), ('id', 235603264683))),
+     ('A.A', (('a', 1), ('id', 0)))]
+    """
 
-    def test_20_dict_to_rels_itr__gen_id(self):
-        dic = dict(a=1)
-        oid = TT._gen_id(('a', 1))
-        self.assertEqual(list(TT._dict_to_rels_itr(dic, "A")),
-                         [('A', (('id', oid), ('a', 1)))])
+    def test_10_with_id_and_relvar(self):
+        dic = dict(a=1, id=0)
+        ref = [('A', (('a', 1), ('id', 0)))]
+        self.assertEqual(_ndict_to_rels(dic, "A"), ref)
 
 # vim:sw=4:ts=4:et:
